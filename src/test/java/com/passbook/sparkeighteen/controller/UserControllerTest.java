7f2b67803775b339bcf5a453f0beca75685dc95d
Kunal Saxena <kunalsaxena@Kunals-MacBook-Pro.local>
2022-06-18 13:00:51 +0530

    minor refactoring

--- src/test/java/com/passbook/sparkeighteen/controller/UserControllerTest.java
+++ src/test/java/com/passbook/sparkeighteen/controller/UserControllerTest.java
@@ -3,7 +3,6 @@ package com.passbook.sparkeighteen.controller;
 import com.passbook.sparkeighteen.peristence.POJO.SignUpRequest;
 import com.passbook.sparkeighteen.peristence.POJO.SignUpResponse;
 import com.passbook.sparkeighteen.service.UserService;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,16 +10,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith({MockitoExtension.class})
