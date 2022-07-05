ced7595515b84bdf002722f710f2a8be853b6b27
kflash06 <ketan.shinde@sparkeighteen.com>
2022-07-05 16:19:33 +0530

    Removed Java Docs from Fields

--- src/main/java/com/passbook/sparkeighteen/peristence/POJO/TransactionRequest.java
+++ src/main/java/com/passbook/sparkeighteen/peristence/POJO/TransactionRequest.java
@@ -19,26 +19,14 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class TransactionRequest {
 
-    /**
-     * amount to be transacted through debit/credit which cannot be null/zero/negative
-     */
     @NotNull(message = "Transaction amount cannot be null")
     @NegativeOrZero(message = "Transaction amount cannot be zero or negative")
     private Float amount;
 
-    /**
-     * name of the person whom you want to transfer or receive amount from
-     */
     private String name;
 
-    /**
-     * note or a description of the transaction you made for example to buy a cold drink.
-     */
     private String note;
 
-    /**
-     * type of transaction to be executed - Credit or Debit
-     */
     @NotNull
     @Enumerated(EnumType.STRING)
     private TransactionType transactionType;
