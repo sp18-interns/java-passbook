fcd423f2eaff3b66cd538373e0245152b1610048
kflash06 <ketan.shinde@sparkeighteen.com>
2022-07-22 23:58:45 +0530

    Kafka-Transaction API

---
+++ src/main/java/com/passbook/sparkeighteen/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.passbook.sparkeighteen.utils;
+
+public class Constants {
+    public static final String TRANSACTION_REQUESTS_TOPIC = "transaction-requests";
+    public static final String GROUP_ID = "group_id";
+
+}
